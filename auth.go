IMQS/authaus

commit 6d70afee61e42febd9336c6d3d4ed73828a78a52

Introduce pseudo UserId values for LDAP/MSAAD/OAuth user merge systems
This is a fix for ticket INF-1369.

The audit trail needs more information about who/what is modifying
a user, which is why we're introducing 3 new pseudo UserId values:

 0: Administrator (this was already present)
-2: LDAP Merge
-3: OAuth Merge
-4: MSAAD Merge

This commit also fixes a host of broken unit tests, so that all tests
now at least pass with the Postgres backend, and dummy backend.

NOTE: The following commit contains the *actual* fix for the bug
mentioned. This commit lays the groundwork for a proper fix.

auth.go
@@ -625,7 +625,10 @@ func (x *Central) CreateSession(user *AuthUser, clientIPAddress, oauthSessionID 
 func (x *Central) authenticate(identity, password string, clientIPAddress string) (AuthUser, error) {
 	user, err := x.userStore.GetUserFromIdentity(identity)
 	if err != nil {
-		return AuthUser{}, fmt.Errorf("%v: %v", ErrIdentityAuthNotFound, err)
+		if !errors.Is(err, ErrIdentityAuthNotFound) {
+			err = fmt.Errorf("%v: %w", ErrIdentityAuthNotFound, err)
+		}
+		return AuthUser{}, err
 	}
 	var authTypeCheck AuthCheck
 	if x.PasswordExpiresAfter != 0 {
@@ -923,8 +926,15 @@ func (x *Central) GetUserFromUserId(userId UserId) (AuthUser, error) {
 
 // GetUserNameFromUserId gets AuthUser full name from userid.
 func (x *Central) GetUserNameFromUserId(userId UserId) string {
-	if userId == 0 {
+	switch userId {
+	case UserIdAdministrator:
 		return "Administrator"
+	case UserIdLDAPMerge:
+		return "LDAP Merge"
+	case UserIdOAuthImplicitCreate:
+		return "OAuth Sign-in"
+	case UserIdMSAADMerge:
+		return "MSAAD Merge"
 	}
 	user, e := x.userStore.GetUserFromUserId(userId)
 	if e == nil {
@@ -985,6 +995,10 @@ func (x *Central) GetRoleGroupDB() RoleGroupDB {
 	return x.roleGroupDB
 }
 
+func (x *Central) IsShuttingDown() bool {
+	return atomic.LoadUint32(&x.shuttingDown) == 0
+}
+
 func (x *Central) Close() {
 	if x.Log != nil {
 		x.Log.Infof("Authaus has started shutting down")
@@ -1019,7 +1033,8 @@ func (x *Central) Close() {
 	}
 	if x.Log != nil {
 		x.Log.Infof("Authaus has shut down")
-		x.Log = nil
+		// Don't set Log to nil, because a background/cleanup goroutine can't be expected to
+		// check for x.Log being nil every time before it emits a log message.
 	}
 }
 

all_test.go
@@ -868,10 +868,10 @@ func TestAuthResetPassword(t *testing.T) {
 	c := setup(t)
 	defer Teardown(c)
 
-	if _, err := c.ResetPasswordStart(notFoundUserId, time.Now()); err != ErrIdentityAuthNotFound {
+	if _, err := c.ResetPasswordStart(notFoundUserId, time.Now()); !errors.Is(err, ErrIdentityAuthNotFound) {
 		t.Fatalf("ResetPasswordStart should fail with ErrIdentityAuthNotFound instead of %v", err)
 	}
-	if err := c.ResetPasswordFinish(joeUserId, "", "12345"); err != ErrInvalidPasswordToken {
+	if err := c.ResetPasswordFinish(joeUserId, "", "12345"); !errors.Is(err, ErrInvalidPasswordToken) {
 		t.Fatalf("ResetPasswordFinish should fail with ErrInvalidPasswordToken instead of %v", err)
 	}
 
@@ -898,13 +898,13 @@ func TestAuthResetPassword(t *testing.T) {
 	if err := c.ResetPasswordFinish(notFoundUserId, token2, "yes"); err != ErrIdentityAuthNotFound {
 		t.Fatalf("ResetPasswordFinish should fail with ErrIdentityAuthNotFound instead of %v", err)
 	}
-	if err := c.ResetPasswordFinish(joeUserId, token1, "yes"); err != ErrInvalidPasswordToken {
+	if err := c.ResetPasswordFinish(joeUserId, token1, "yes"); !errors.Is(err, ErrInvalidPasswordToken) {
 		t.Fatalf("ResetPasswordFinish on dead token should fail with ErrInvalidPasswordToken instead of %v", err)
 	}
 	if err := c.ResetPasswordFinish(joeUserId, token2, "12345"); err != nil {
 		t.Fatalf("ResetPasswordFinish should succeed instead of %v", err)
 	}
-	if err := c.ResetPasswordFinish(joeUserId, token2, "12345"); err != ErrInvalidPasswordToken {
+	if err := c.ResetPasswordFinish(joeUserId, token2, "12345"); !errors.Is(err, ErrInvalidPasswordToken) {
 		t.Fatalf("ResetPasswordFinish a 2nd time should fail with ErrInvalidPasswordToken instead of %v", err)
 	}
 	if token, err := c.GetTokenFromIdentityPassword(joeEmail, joePwd); err == nil || token != nil {
@@ -919,7 +919,7 @@ func TestAuthResetPassword(t *testing.T) {
 
 	// Test time expiry
 	token3, _ := c.ResetPasswordStart(joeUserId, time.Now().Add(-3*time.Second))
-	if err := c.ResetPasswordFinish(joeUserId, token3, "12345"); err != ErrPasswordTokenExpired {
+	if err := c.ResetPasswordFinish(joeUserId, token3, "12345"); !errors.Is(err, ErrPasswordTokenExpired) {
 		t.Fatalf("ResetPasswordFinish should have failed with ErrPasswordTokenExpired instead of %v", err)
 	}
 
@@ -1300,8 +1300,8 @@ func TestAuthUpdateIdentity(t *testing.T) {
 		ModifiedBy:      newModifiedBy,
 		Type:            UserTypeLDAP,
 	}
-	if err := c.UpdateIdentity(&notFoundUser); err != ErrIdentityAuthNotFound {
-		t.Fatalf("TestUpdateIdentity failed: Expected ErrIdentityAuthNotFound, but got: %v", err)
+	if err := c.UpdateIdentity(&notFoundUser); err == nil {
+		t.Fatalf("Expected ErrIdentityAuthNotFound, but got: %v", err)
 	}
 
 	joeUser := AuthUser{
@@ -1387,7 +1387,7 @@ func TestAuthArchiveIdentity(t *testing.T) {
 
 	now := time.Now().UTC()
 
-	if err := c.ArchiveIdentity(notFoundUserId); err != ErrIdentityAuthNotFound {
+	if err := c.ArchiveIdentity(notFoundUserId); !errors.Is(err, ErrIdentityAuthNotFound) {
 		t.Fatalf("TestArchiveIdentity failed: Expected ErrIdentityAuthNotFound, but got: %v", err)
 	}
 
@@ -1430,8 +1430,8 @@ func TestAuthArchiveIdentity(t *testing.T) {
 	// Using an ldap backend, authentication will succeed, as we cannot archive users on the ldap system.
 	if !*backend_ldap {
 		// Try to authenticate with archived user
-		if _, err := c.GetTokenFromIdentityPassword(joeEmail, joePwd); err != ErrIdentityAuthNotFound {
-			t.Fatalf("TestArchiveIdentity failed, archived user should not be allowed to authenticate: %v", err)
+		if _, err := c.GetTokenFromIdentityPassword(joeEmail, joePwd); err == nil {
+			t.Fatalf("Archived user should not be allowed to authenticate: %v", err)
 		}
 	}
 
@@ -1449,18 +1449,18 @@ func TestAuthArchiveIdentity(t *testing.T) {
 		ModifiedBy:      0,
 		Type:            UserTypeDefault,
 	}
-	if err := c.UpdateIdentity(&joeUser); err != ErrIdentityAuthNotFound {
-		t.Fatalf("TestArchiveIdentity failed, archived user should not be allowed to be updated: %v", err)
+	if err := c.UpdateIdentity(&joeUser); err == nil {
+		t.Fatalf("Archived user should not be allowed to be updated: %v", err)
 	}
 
 	// Try resetting password of archived user
-	if _, err := c.ResetPasswordStart(joeUserId, time.Now()); err != ErrIdentityAuthNotFound {
-		t.Fatalf("TestArchiveIdentity failed, archived user should not be allowed to be reset password: %v", err)
+	if _, err := c.ResetPasswordStart(joeUserId, time.Now()); err == nil {
+		t.Fatalf("Archived user should not be allowed to be reset password: %v", err)
 	}
 
 	// Try renaming email of archived user
-	if err := c.RenameIdentity(joeEmail, "newJoe"); err != ErrIdentityAuthNotFound {
-		t.Fatalf("TestArchiveIdentity failed, archived user should not be allowed to be rename identity: %v", err)
+	if err := c.RenameIdentity(joeEmail, "newJoe"); err == nil {
+		t.Fatalf("Archived user should not be allowed to be rename identity: %v", err)
 	}
 }
 

db.go
@@ -49,6 +49,21 @@ const (
 
 type UserId int64
 
+const (
+	// The value 0 was originally placed in the CreatedBy and ModifiedBy fields of a user's record,
+	// when that user was created or modified by the LDAP sync process. Later, we decided to
+	// formalize this, which is when UserIdLDAPMerge was born.
+	// These constants are embedded inside our DB, in the CreatedBy and ModifiedBy fields of
+	// a user's record, so they may not change.
+	UserIdAdministrator UserId = 0
+	// skip -1, because it's such a frequent "invalid" integer code
+	UserIdLDAPMerge           = -2 // Created/Modified by LDAP integration
+	UserIdOAuthImplicitCreate = -3 // Created implicitly by OAuth sign-in
+	UserIdMSAADMerge          = -4 // Created/Modified by MSAAD integration
+	// NOTE:
+	// If you add to this list, be sure to update GetUserNameFromUserId() too
+)
+
 type AuthUserType int
 
 func (u AuthUserType) CanSetPassword() bool {

db_sql.go
@@ -188,7 +188,7 @@ func (x *sqlUserStoreDB) ResetPasswordStart(userId UserId, expires time.Time) (s
 	var tx *sql.Tx
 	var err error
 	if tx, err = x.db.Begin(); err != nil {
-		return "", fmt.Errorf("Could not begin transaction: %v", err)
+		return "", fmt.Errorf("Could not begin transaction: %w", err)
 	}
 	defer tx.Rollback()
 
@@ -202,15 +202,15 @@ func (x *sqlUserStoreDB) ResetPasswordStart(userId UserId, expires time.Time) (s
 		token, userId, UserTypeDefault,
 	)
 	if err != nil {
-		return "", fmt.Errorf("Could not execute update statement (ResetPasswordStart): %v", err.Error())
+		return "", fmt.Errorf("Could not execute update statement (ResetPasswordStart): %w", err)
 	}
 	if affected, err := update.RowsAffected(); err != nil {
-		return "", fmt.Errorf("Could not execute find UserID record (ResetPasswordStart): %v", err.Error())
+		return "", fmt.Errorf("Could not execute find UserID record (ResetPasswordStart): %w", err)
 	} else if affected != 1 {
-		return "", fmt.Errorf("Could not find UserID record (ResetPasswordStart): %v", ErrIdentityAuthNotFound)
+		return "", fmt.Errorf("Could not find UserID record (ResetPasswordStart): %w", ErrIdentityAuthNotFound)
 	}
 	if err := tx.Commit(); err != nil {
-		return "", fmt.Errorf("Could not commit transaction: %v", err)
+		return "", fmt.Errorf("Could not commit transaction: %w", err)
 	}
 	return token, nil
 }
@@ -231,22 +231,22 @@ func (x *sqlUserStoreDB) ResetPasswordFinish(userId UserId, token string, passwo
 		if err == sql.ErrNoRows {
 			return ErrIdentityAuthNotFound
 		}
-		return fmt.Errorf("Could not read pwdtoken: %v", err)
+		return fmt.Errorf("Could not read pwdtoken: %w", err)
 	}
 	if err = verifyPasswordResetToken(token, truthToken.String); err != nil {
-		return fmt.Errorf("Could not verify password reset token: %v", err)
+		return fmt.Errorf("Could not verify password reset token: %w", err)
 	}
 	if enforceTypeCheck&PasswordEnforcementReuse != 0 && x.hasPasswordBeenUsedBefore(userId, password) {
 		return ErrInvalidPastPassword
 	}
 	if err = x.archivePassword(tx, userId); err != nil {
-		return fmt.Errorf("Could not archive password: %v", err)
+		return fmt.Errorf("Could not archive password: %w", err)
 	}
 	if err = x.setPasswordInternal(tx, userId, password); err != nil {
-		return fmt.Errorf("Could not reset password: %v", err)
+		return fmt.Errorf("Could not reset password: %w", err)
 	}
 	if err = tx.Commit(); err != nil {
-		return fmt.Errorf("Could not commit transaction: %v", err)
+		return fmt.Errorf("Could not commit transaction: %w", err)
 	}
 	return nil
 
@@ -493,13 +493,13 @@ func (x *sqlUserStoreDB) ArchiveIdentity(userId UserId) error {
 	defer tx.Rollback()
 	update, err := tx.Exec(`UPDATE authuserstore SET archived = $1 WHERE userid = $2`, true, userId)
 	if err != nil {
-		return fmt.Errorf("Could not update auth user: %v", err)
+		return fmt.Errorf("Could not update auth user: %w", err)
 	}
 	if affected, _ := update.RowsAffected(); affected != 1 {
-		return fmt.Errorf("User could not be updated: %v", ErrIdentityAuthNotFound)
+		return fmt.Errorf("User could not be updated: %w", ErrIdentityAuthNotFound)
 	}
 	if err = tx.Commit(); err != nil {
-		return fmt.Errorf("Could not commit transaction: %v", err)
+		return fmt.Errorf("Could not commit transaction: %w", err)
 	}
 	return nil
 }

msaad.go
@@ -25,7 +25,6 @@ import (
 	"net/url"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -371,6 +370,7 @@ func (m *MSAAD) SynchronizeUsers() error {
 				} else {
 					m.parent.Log.Infof("MSAAD update user %v %v %v", aadUser.profile.DisplayName, aadEmail, aadUser.profile.ID)
 					existingUsers[ix].Modified = time.Now()
+					existingUsers[ix].ModifiedBy = UserIdMSAADMerge
 					if err := m.parent.userStore.UpdateIdentity(&existingUsers[ix]); err != nil {
 						m.parent.Log.Warnf("MSAAD: Update user %v failed: %v", aadUser.profile.ID, err)
 					}
@@ -386,6 +386,8 @@ func (m *MSAAD) SynchronizeUsers() error {
 				user := aadUser.profile.toAuthUser()
 				user.Created = time.Now()
 				user.Modified = user.Created
+				user.CreatedBy = UserIdMSAADMerge
+				user.ModifiedBy = UserIdMSAADMerge
 				if newUserID, err := m.parent.userStore.CreateIdentity(&user, ""); err != nil {
 					m.parent.Log.Warnf("MSAAD: Create identity %v failed: %v", aadEmail, err)
 				} else {
@@ -740,7 +742,7 @@ func (m *MSAAD) populateAADRoles(users []*msaadUser) error {
 				// users on this AAD, then it certainly pays to parallelize these fetches.
 				selectURL := "https://graph.microsoft.com/v1.0/users/" + user.profile.ID + "/appRoleAssignments"
 				for selectURL != "" {
-					if atomic.LoadUint32(&m.parent.shuttingDown) != 0 {
+					if m.parent.IsShuttingDown() {
 						break
 					}
 					j := msaadRolesJSON{}

oauth.go
@@ -149,7 +149,7 @@ func (x *OAuth) Initialize(parent *Central) {
 	go func() {
 		// Startup grace
 		time.Sleep(5 * time.Second)
-		for {
+		for !x.parent.IsShuttingDown() {
 			x.purgeUnusedOAuthSessions()
 			time.Sleep(time.Minute)
 		}
@@ -165,7 +165,7 @@ func (x *OAuth) Initialize(parent *Central) {
 			}
 			// Startup grace
 			time.Sleep(5 * time.Second)
-			for {
+			for !x.parent.IsShuttingDown() {
 				x.validateTokens()
 				time.Sleep(time.Duration(interval) * time.Second)
 			}
@@ -846,8 +846,8 @@ func (x *OAuth) createOrGetUserID(profile *OAuthUserProfile) (UserId, error) {
 		Modified:     time.Now().UTC(),
 		Type:         UserTypeOAuth,
 		ExternalUUID: profile.UUID,
-		//CreatedBy            UserId
-		//ModifiedBy           UserId
+		CreatedBy:    UserIdOAuthImplicitCreate,
+		ModifiedBy:   UserIdOAuthImplicitCreate,
 		//Archived             bool
 		//PasswordModifiedDate time.Time
 		//AccountLocked        bool

commit 46f19842ae174709ce5122387696b58c608a52ba

Fix broken IsShuttingDown() (logic was reversed) - INF-1380
This fixes INF-1380. I introduced this regression with commit
6d70afee61e42febd9336c6d3d4ed73828a78a52

auth.go
@@ -996,7 +996,7 @@ func (x *Central) GetRoleGroupDB() RoleGroupDB {
 }
 
 func (x *Central) IsShuttingDown() bool {
-	return atomic.LoadUint32(&x.shuttingDown) == 0
+	return atomic.LoadUint32(&x.shuttingDown) != 0
 }
 
 func (x *Central) Close() {