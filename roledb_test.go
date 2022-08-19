b63e579fb8076cb0fae35cb2a55a45ba7a4982f1
JacoV <jvosloo@imqs.co.za>
2022-08-19 20:12:08 +0200

    ASG-1990 : Auth fails if a group is not found in a user permit.
    Enhance GroupIDsToName and PermitResolveToList to return best effort plus errors.
    New helper function 'addAllGroupNamesToCache'
    Add MSAAD helper function idToGroupName
    Enhance RoleDB tests
    
    Update docs/changelog.md
    Patch vulnerabilities in golang.org/x/crypto
    
    Co-authored-by: Fritz <fritz.engl@gmail.com>

--- roledb_test.go
+++ roledb_test.go
@@ -1,6 +1,8 @@
 package authaus
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -10,11 +12,13 @@ import (
 const (
 	permX PermissionU16 = 1
 	permY PermissionU16 = 2
+	permZ PermissionU16 = 3
 )
 
 const (
 	groupNameXandY = "groupXandY"
 	groupNameY     = "groupY"
+	groupNameZ     = "groupZ"
 )
 
 func setup1_withRoleDB(t *testing.T) *Central {
@@ -23,15 +27,29 @@ func setup1_withRoleDB(t *testing.T) *Central {
 	groupY := &AuthGroup{}
 	groupY.Name = groupNameY
 	groupY.AddPerm(permY)
-	c.GetRoleGroupDB().InsertGroup(groupY)
-
+	e := c.GetRoleGroupDB().InsertGroup(groupY)
+	if e != nil {
+		assert.Fail(t, "Could not add groups.")
+	}
 	groupXandY := &AuthGroup{}
 	groupXandY.Name = groupNameXandY
 	groupXandY.AddPerm(permX)
 	groupXandY.AddPerm(permY)
-	c.GetRoleGroupDB().InsertGroup(groupXandY)
-
+	e = c.GetRoleGroupDB().InsertGroup(groupXandY)
+	if e != nil {
+		assert.Fail(t, "Could not add groups.")
+	}
 	return c
+}
+
+func setup1_AddRoleGroup(t *testing.T, c *Central) {
+	groupZ := &AuthGroup{}
+	groupZ.Name = groupNameZ
+	groupZ.AddPerm(permZ)
+	e := c.GetRoleGroupDB().InsertGroup(groupZ)
+	if e != nil {
+		assert.Fail(t, "Could not add groups.")
+	}
 }
 
 func TestAuthRoleDB(t *testing.T) {
@@ -60,4 +78,60 @@ func TestAuthRoleDB(t *testing.T) {
 	}
 	go fetchAllGroups()
 	go fetchAllGroups()
+}
+
+func TestAuthRoleDB_MissingGroups(t *testing.T) {
+	c := setup1_withRoleDB(t)
+	roleGroupDBCache := c.roleGroupDB.(*RoleGroupCache)
+
+	idu32s := make([]GroupIDU32, 3)
+	idu32s[0] = GroupIDU32(1)
+	idu32s[1] = GroupIDU32(2)
+	idu32s[2] = GroupIDU32(3)
+	pbyte := EncodePermit(idu32s)
+	plist, e := PermitResolveToList(pbyte, roleGroupDBCache)
+	fmt.Printf("Permissions: %v\n", plist)
+	assert.NotNil(t, e, "An error should be returned for the missing group")
+	assert.Equal(t, 2, len(plist))
+}
+
+func TestAuthRoleDB_GroupIdsToNames(t *testing.T) {
+	c := setup1_withRoleDB(t)
+
+	// Normal case
+	idu32s := make([]GroupIDU32, 2)
+	idu32s[0] = GroupIDU32(1)
+	idu32s[1] = GroupIDU32(2)
+
+	cache := map[GroupIDU32]string{}
+	plist, e := GroupIDsToNames(idu32s, c.roleGroupDB, cache)
+
+	fmt.Printf("Permissions: %v\n", plist)
+	assert.Nil(t, e, "Error is not expected.")
+	assert.Equal(t, 2, len(plist), "Invalid nr of permissions in list.")
+	assert.Equal(t, 2, len(cache), "Invalid nr of cache items in list.")
+
+	// Missing group
+	idu32s = make([]GroupIDU32, 3)
+	idu32s[0] = GroupIDU32(1)
+	idu32s[1] = GroupIDU32(2)
+	idu32s[2] = GroupIDU32(3)
+	plist, e = GroupIDsToNames(idu32s, c.roleGroupDB, cache)
+
+	fmt.Printf("Permissions: %v\n", plist)
+	assert.NotNil(t, e, "Error is expected.")
+	assert.Equal(t, 2, len(plist), "Invalid nr of permissions in list. round 2.")
+	assert.Equal(t, 2, len(cache), "Invalid nr of cache items in list. round 2.")
+
+	// Rectify missing group
+	setup1_AddRoleGroup(t, c)
+	cache = map[GroupIDU32]string{}
+	idu32s2 := make([]GroupIDU32, 1)
+	idu32s2[0] = GroupIDU32(3)
+
+	// re-use the existing local cache
+	plist, e = GroupIDsToNames(idu32s2, c.roleGroupDB, cache)
+	fmt.Printf("Permissions: %v\n", plist)
+	assert.Equal(t, 1, len(plist), "Invalid nr of permissions in list, round 3.")
+	assert.Equal(t, 3, len(cache), "Invalid nr of cache items in list, round 3.")
 }
